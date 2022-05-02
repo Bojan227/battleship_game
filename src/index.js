265ae2b18b8f7e1a04db0f2e9552abd4110d2d44
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-05-02 12:47:08 +0200

    Handle attacks for the user

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const gameFlow = (() => {
     const container = document.querySelector('.container');
     const mainBoard = document.querySelector('.user-board');
     const enemyBoard = document.querySelector('.enemy-board');
+    const startGameButton = document.querySelector('.start-game');
+    const winMsg = document.querySelector('.win-msg');
 
     const mainPlayerCoord = [
         [0, 5, 3, 'h'],
@@ -30,4 +32,53 @@ const gameFlow = (() => {
 
     displayShips(mainPlayerCoord);
     console.log(mainPlayer.getArray());
+
+    const handleAttacks = () => {
+        container.addEventListener('click', (e) => {
+            if (e.target.className === 'container') return;
+            if (e.target.parentElement.className === 'user-board') return;
+            const x = parseInt(e.target.dataset.x);
+            const y = parseInt(e.target.dataset.y);
+
+            console.log(x, y);
+
+            if (e.target.parentElement.className === 'enemy-board') {
+                const xAttack = document.createElement('h3');
+                xAttack.classList.add('x');
+
+                const emptyAttack = document.createElement('h3');
+                emptyAttack.classList.add('empty-attack');
+
+                computerPlayer.receiveAttack(x, y);
+
+                if (e.target.dataset.ship !== '') {
+                    e.target.append(xAttack);
+                    e.target.style.border = '2px solid #991B1B';
+                } else if (e.target.dataset.ship === '') {
+                    e.target.append(emptyAttack);
+                    e.target.style.backgroundColor = '#E4E4E7';
+                }
+
+                computerPlayer.checkWin(winMsg, 'Computer');
+
+                // computer moves
+                // const [coordX, coordY] = generateRandomNumbers(enemyCoord);
+
+                // mainPlayer.receiveAttack(coordX, coordY);
+
+                // mainBoard.innerHTML = '';
+                // displayEnemyAttacks(mainPlayer.getArray(), mainBoard);
+                // displayShips(mainPlayerCoord);
+                // mainPlayer.checkWin(winMsg, 'User');
+            }
+        });
+    };
+
+    startGameButton.addEventListener('click', () => {
+        // updateGameboard();
+        // startGameMsg.dataset.status = 'disable';
+        // startGameMsg.classList.add('invisible');
+        // startGameButton.classList.add('invisible');
+        handleAttacks();
+    });
 })();
