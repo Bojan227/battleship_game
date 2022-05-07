d8bae636c1b2b67d6231f488dadd5f44f6ef098a
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-05-07 16:35:32 +0200

    Generate random coordinates

---
+++ src/computerMoves.js
@@ -0,0 +1,15 @@
+function generateRandomNumbers(mainCoord) {
+    const coord = [];
+    for (let i = 0; i < 2; i += 1) {
+        coord.push(Math.floor(Math.random() * 10));
+    }
+    if (
+        mainCoord.some(
+            (element) => JSON.stringify(element) === JSON.stringify(coord)
+        )
+    ) {
+        return generateRandomNumbers(mainCoord);
+    }
+    mainCoord.push(coord);
+    return coord;
+}
